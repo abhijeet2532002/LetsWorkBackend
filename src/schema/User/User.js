8850b63bd0250d8cb9ffea07df17b7a1421b6d58
Shubham Shrivastwa <kumarraunak4201@gmail.com>
2024-10-22 13:19:05 -0700

    user model

---
+++ src/schema/User/User.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+const userSchema = new mongoose.Schema({
+    userName:{type:String, required:true},
+    userEmail:{type:String,required:true},
+    userPassword:{type:String,required:true},
+    userRole:{type:String, default:'client'},
+    pastProjects:[],
+    currentProject:[],
+    userAddress:{type:String, required:true},
+    userProffesion:{type:String},
+    currentDocument:[],
+    Contribution:{type:String},
+    CurrentLink:{type:String},
+    isBlocked:{type: Boolean, default:false},
+    feedback:{type:String},
+    Payment:{type:String, enum:['pending','Success','fail','half','full']},
+    amount:{type:String},
+
+}, { timestamps: true });
+
+const User = mongoose.model("User",userSchema);
+
+export default User;
