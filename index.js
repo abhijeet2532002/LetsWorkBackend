a4103bcc980c4783da2f82010b5e3238e4eae262
abhijeet2532002 <abhijeetkumar2532002@gmail.com>
2024-10-22 21:30:34 +0530

    add blog

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ import DB from './configuration/DB_Config/MongoDBConfig.js';
 import MainRouter from './router/mainRouter.js'
 
 const app = express();
+app.use(express.json());
 dotenv.config();
 const port = process.env.PORT || 8000;
 
-app.use('/', MainRouter);
+app.use('/letswork', MainRouter);
 
 app.listen(port, () => {
     console.log(`app is successfully running on PORT Number is ${port}`);
--- schema/Blog/BlogSchema.js
+++ schema/Blog/BlogSchema.js
@@ -2,8 +2,7 @@ import mongoose from "mongoose";
 
 const schema = new mongoose.Schema({
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
+        type: String
     },
     title: {
         type: String,
