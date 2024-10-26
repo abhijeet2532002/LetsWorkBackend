01c84b22d3ab4a2d305897fd985d8745b110b480
abhijeet2532002 <abhijeetkumar2532002@gmail.com>
2024-10-26 13:07:33 +0530

    add blog

--- src/controller/Blog/BlogController.js
+++ src/controller/Blog/BlogController.js
@@ -1,6 +1,7 @@
 import BlogSchema from "../../schema/Blog/BlogSchema.js"
 import { v2 as cloudinary } from 'cloudinary';
 
+
 import formidable from 'formidable';
 
 import dotenv from 'dotenv';
@@ -16,40 +17,28 @@ export default class BlogContainer {
     // Add blog
     addBlog = async (req, res) => {
         const form = formidable();
-
-        // Object to store parsed fields
         const parsedFields = {};
         form.on('field', (name, value) => {
             parsedFields[name] = value;
         });
 
         form.parse(req, async (err, fields, files) => {
+            console.log(fields, files);
+
             if (err) {
                 return res.status(400).json({ message: 'Error parsing form data', err });
             }
             try {
-                // Uploading the image to Cloudinary
-                const image = await cloudinary.uploader.upload(files.image[0].filepath);
-                parsedFields.image = image.url;
-
-                // Store blog data 
-                const data = {
-                    user: parsedFields.user,
-                    title: parsedFields.title,
-                    description: parsedFields.description,
-                    content: parsedFields.content,
-                    imageUrl: parsedFields.image,
-                    imagePublicId: image.public_id,
-                    imageSecureUrl: image.secure_url
-                }
-                const blogData = await BlogSchema.create(data);
-
-                // Response
-                res.status(201).json({
+                const image = await cloudinary.uploader.upload(files?.image[0]?.filepath);
+                parsedFields.imageUrl = image.url;
+                parsedFields.imagePublicId = image.public_id;
+                parsedFields.imageSecureUrl = image.secure_url;
+                const blogData = await BlogSchema.create(parsedFields);
+                return res.status(201).json({
                     blogData
                 });
             } catch (error) {
-                res.status(400).json({ message: 'Error creating blog', error });
+                return res.status(400).json({ message: 'Error creating blog', error });
             }
         });
     };
@@ -114,15 +103,12 @@ export default class BlogContainer {
     updateBlogId = async (req, res) => {
         try {
             const blogData = await BlogSchema.findById(req.params.id);
-            console.log(req.params.id);
-
             if (!blogData)
                 return res.status(404).json({
                     message: "Data is not available"
                 })
             else {
                 const form = formidable();
-
                 // Object to store parsed fields
                 const parsedFields = {};
                 form.on('field', (name, value) => {
@@ -157,17 +143,14 @@ export default class BlogContainer {
                     };
 
                     // Update the blog entry in the database
-                    const updatedBlog = await BlogSchema.findByIdAndUpdate(req.params.id, updateData, {
-                        new: true,
-                        runValidators: true
-                    });
-
-                    if (!updatedBlog) {
-                        return res.status(404).json({ message: 'Blog not found' });
-                    }
+                    const updatedBlog = await BlogSchema.findByIdAndUpdate(req.params.id, updateData,
+                        {
+                            new: true,
+                            runValidators: true
+                        });
 
                     // Respond with the updated blog data
-                    res.status(200).json({
+                    return res.status(200).json({
                         message: 'Blog updated successfully',
                         blog: updatedBlog
                     });
--- src/index.js
+++ src/index.js
@@ -2,18 +2,12 @@ import express from 'express';
 import dotenv from 'dotenv';
 import DB from './config/DB_Config/MongoDBConfig.js';
 
-
-
-
-
-
-
 import MainRouter from './router/mainRouter.js'
 const app = express();
 dotenv.config();
 const port = process.env.PORT || 8000;
-app.use(express.urlencoded({extended:true}));
-app.use(express.json());``
+// app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 app.use('/letswork', MainRouter);
 
