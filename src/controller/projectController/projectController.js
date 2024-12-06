15dc7b5c328d5e1470142808883493e6b8657ace
Shubham Shrivastwa <kumarraunak4201@gmail.com>
2024-12-06 07:21:22 -0800

    project

---
+++ src/controller/projectController/projectController.js
@@ -0,0 +1,28 @@
+import Project from '../../schema/Project/Project.js';
+
+class projectController{
+
+    addProject = async (req,res)=>{
+
+        return res.json(req.body)
+
+    }
+
+    getAllProject = async (req,res)=>{
+
+    }
+
+    updateProject = async (req,res)=>{
+
+    }
+
+    deleteProject = async (req,res)=>{
+
+    }
+
+    singleProject = async (req,res)=>{
+
+    }
+}
+
+export default new projectController();
--- src/router/projectRouter/projectRouter.js
+++ src/router/projectRouter/projectRouter.js
@@ -1,2 +1,13 @@
 import express from 'express';
+import projectController from '../../controller/projectController/projectController.js';
+const {addProject,deleteProject,getAllProject,singleProject,updateProject} = projectController;
+
+const router = express.Router();
+router.post('/add/data',addProject);
+router.get('/getAll/data',getAllProject);
+router.delete('/delete/data',deleteProject);
+router.get('/fetch/data/:id',singleProject);
+router.patch('/update/:id',updateProject);
+
+export default router;
 
