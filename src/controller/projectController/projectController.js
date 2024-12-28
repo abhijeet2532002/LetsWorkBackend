c742575193b61d6d593da06e9de6ad4d44d8fbfe
Shubham Shrivastwa <kumarraunak4201@gmail.com>
2024-12-28 11:24:13 +0530

    Add Wizard API

--- src/controller/projectController/projectController.js
+++ src/controller/projectController/projectController.js
@@ -1,42 +1,84 @@
-import Project from '../../schema/Project/Project.js';
-import User from '../../schema/User/User.js';
+import Project from "../../schema/Project/Project.js";
+import User from "../../schema/User/User.js";
 
 class projectController {
-
-    addProject = async (req, res) => {
-        const { userEmail } = req.body;
-        try {
-            const client = await User.findOne({userEmail});
-            if (!client) {
-                return res.status(404).json({Error:`user does't exist with ${userEmail}`})
-            }
-            const team = await User.find({ userRole: 'admin' },'_id');
-            const projectModel = await Project.create(req.body);
-            projectModel.teams.push(...team.map(admin => admin._id));
-            projectModel.save();
-            return res.status(200).json(projectModel);
-            
-        } catch (err) {
-           return res.status(500).json(err)
-        }
-
+  addProject = async (req, res) => {
+    const { userEmail } = req.body;
+    try {
+      const client = await User.findOne({ userEmail });
+      if (!client) {
+        return res
+          .status(404)
+          .json({ Error: `user does't exist with ${userEmail}` });
+      }
+      const team = await User.find({ userRole: "admin" }, "_id");
+      const projectModel = await Project.create(req.body);
+      projectModel.teams.push(...team.map((admin) => admin._id));
+      projectModel.save();
+      return res.status(200).json(projectModel);
+    } catch (err) {
+      return res.status(500).json(err);
     }
+  };
 
-    getAllProject = async (req, res) => {
-
+  getAllProject = async (req, res) => {
+    try {
+      return res.status(200).json(await Project.find({}));
+    } catch (err) {
+      return res.status(501).json(err);
     }
+  };
 
-    updateProject = async (req, res) => {
-
+  updateProject = async (req, res) => {
+    try {
+      const updateProject = await Project.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+      );
+      return res.status(200).json({ Message: updateProject });
+    } catch (err) {
+      return res.status(500).json(err);
     }
+  };
 
-    deleteProject = async (req, res) => {
+  deleteProject = async (req, res) => {
+    try {
+      const project = await Project.findByIdAndDelete(req.params.id);
+      if (project) {
+        return res
+          .status(200)
+          .json({ Message: `Product deleted with ID ${project.id}` });
+      }
+      return res
+        .status(401)
+        .json({ MessageError: `User Does't exist with id ${req.params.id}` });
+    } catch {}
+  };
 
+  singleProject = async (req, res) => {
+    try {
+      const project = await Project.findById(req.params.id);
+      if (project) {
+        return res.status(200).json(project);
+      }
+      return res.status(401).json({
+        MessageError: `User Does't exist with id ${req.params.id}`,
+      });
+    } catch {}
+  };
+
+  updateWizard = async (req, res) => {
+    try {
+      console.log("hello");
+      let project = await Project.findById(req.params.id);
+      project.wizard = {...project.wizard,...req.body}
+      project.save();
+      return res.status(200).json(project);
+    } catch (err) {
+      return res.status(500).json(err);
     }
-
-    singleProject = async (req, res) => {
-
-    }
+  };
 }
 
 export default new projectController();
--- src/router/Auth/AuthRouter.js
+++ src/router/Auth/AuthRouter.js
@@ -3,7 +3,7 @@ import CreateSeasion from "../../Auth/CreateSeasion.js";
 import verifyToken from "../../Auth/Verifytoken.js";
 const { createToken,logout} = CreateSeasion;
 const router = express.Router();
-router.get("/create/session", createToken);
+router.post("/create/session", createToken);
 router.get('/logout',verifyToken,logout);
 
 export default router;
